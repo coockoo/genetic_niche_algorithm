b3bf640b094f07b347f1a02e5e5785a03a2523aa
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-19 07:16:47 +0300

    Refactored equal-maxima function

---
+++ functions/equal-maxima.js
@@ -0,0 +1,19 @@
+var _ = require('lodash');
+
+function equalMaxima (array) {
+	if (array.length === 0) {
+		throw new Error('Cannot process function for zero-length array.');
+	}
+	var sum = _.reduce(array, function (sum, item) {
+		return sum + equalMaximaNumber(item);
+	}, 0);
+	return (1 / array.length) * sum;
+}
+
+function equalMaximaNumber (x) {
+	return Math.pow(Math.sin(5 * Math.PI * x), 6);
+}
+
+console.log(equalMaxima([0.1, 0.3, 0.5]));
+
+module.exports = equalMaxima;
--- functions/fitness/euqal-maxima.js
+++
@@ -1,5 +0,0 @@
-function equalMaxima (x) {
-	return Math.pow(Math.sin(5 * Math.PI * x), 6);
-}
-
-module.exports = equalMaxima;
