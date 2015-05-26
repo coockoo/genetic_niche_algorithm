87d4aadb824cd028c9b1028ffa2bba386bf09ce8
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-26 09:50:21 +0300

    Refactored average fitness according to the new chromosome design

--- genetic/fitness/average.js
+++ genetic/fitness/average.js
@@ -1,7 +1,7 @@
 module.exports = function averageFitness (params) {
 	var sum = 0;
 	for (var i = 0; i < params.population.getSize(); ++i) {
-		sum += params.fitness(params.population.getChromosome(i).getVariables());
+		sum += params.population.getChromosome(i).getFitness();
 	}
 	return sum / params.population.getSize();
 };
