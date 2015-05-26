4017f5da41c2a3d4708e425e625b6b4fee945d3c
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-26 09:33:58 +0300

    Added combinations helper

---
+++ genetic/helpers/combinations.js
@@ -0,0 +1,12 @@
+function combinations (values, n) {
+	if (n === 0) {
+		return [ [] ];
+	}
+	return _(combinations(values, n-1)).map(function (arr) {
+		return _(values).map(function (val) {
+			return arr.concat([val]);
+		}).valueOf();
+	}).flatten().valueOf();
+}
+
+module.exports = combinations;
