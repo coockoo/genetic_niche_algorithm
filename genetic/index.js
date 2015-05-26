1105cd73ca69f70c0f19e339a9b294090fd0606b
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-26 09:57:36 +0300

    Refactored genetic module

--- genetic/index.js
+++ genetic/index.js
@@ -1,5 +1,3 @@
-var averageFitness = require('./fitness/average');
-
 // TODO: see how to document module properly
 
 /**
@@ -9,16 +7,21 @@ var averageFitness = require('./fitness/average');
  * @param options.population Input population to process
  * @param options.sigma Sigma parameter used in the mutation
  * @param options.reproduction Reproduction function to create new generation
- * @param options.fitness Fitness function to be used
  * @param options.mutation Mutation function to be used
  * @param options.stopFitnessThreshold Threshold value to determine algorithm stop
  * @param options.maxGenerations Number after which we stop reproducing
+ *
+ * @param options.onGenerationChanged Function fired on generation changed
  */
 
 module.exports = function genetic (options) {
+
 	if (!options.population) {
 		throw new Error('Cannot run genetic algorithm without population');
 	}
+
+	var stopGenerationsThreshold = 5;
+
 	var results = {
 		generations: 0,
 		finalAverageFitness: null,
@@ -30,26 +33,23 @@ module.exports = function genetic (options) {
 
 	function run () {
 
-		var currentPopulation = options.population;
 		results.generations = 0;
-		var prevAverageFitness = 0;
-		var currentAverageFitness = averageFitness({ population: options.population, fitness: options.fitness });
-		var stopCounter = 0;
-		while (
-			// TODO: maybe remove hardcode ?
-			stopCounter < 5 &&
-			results.generations <= options.maxGenerations
-			) {
 
-			++results.generations;
+		var currentPopulation = options.population;
+		var currentAverageFitness = currentPopulation.getAverageFitness();
+		var prevAverageFitness = null;
+		var stopCounter = 0;
+
+		while ( stopCounter < stopGenerationsThreshold && ++results.generations < options.maxGenerations ) {
+
 			currentPopulation = options.reproduction({
 				population: currentPopulation,
 				sigma: options.sigma,
-				mutation: options.mutation,
-				fitness: options.fitness
+				mutation: options.mutation
 			});
+
 			prevAverageFitness = currentAverageFitness;
-			currentAverageFitness = averageFitness({ population: currentPopulation, fitness: options.fitness });
+			currentAverageFitness = currentPopulation.getAverageFitness();
 
 			if ((currentAverageFitness - prevAverageFitness) > options.stopFitnessThreshold) {
 				stopCounter = 0;
@@ -58,14 +58,11 @@ module.exports = function genetic (options) {
 			}
 		}
 
-		results.finalAverageFitness = currentAverageFitness.toFixed(5);
-		results.finalFitnessDifference = (currentAverageFitness - prevAverageFitness).toFixed(5);
-		results.bestChromosome = currentPopulation.getBestChromosome({ fitness: options.fitness });
+		results.finalAverageFitness = currentAverageFitness;
+		results.finalFitnessDifference = (currentAverageFitness - prevAverageFitness);
+		results.population = currentPopulation;
 
-		console.log('%j', results);
-
-		// TODO: what to return
-		return currentPopulation;
+		return results;
 
 	}
 };
