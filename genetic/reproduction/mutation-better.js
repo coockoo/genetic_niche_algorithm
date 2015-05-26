e6324bf93747163a010b6eb92f104f1805061cec
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-26 09:57:01 +0300

    Refactored mutation-better reproduction according to the new chromosome design

--- genetic/reproduction/mutation-better.js
+++ genetic/reproduction/mutation-better.js
@@ -7,20 +7,20 @@ var population = require('../population');
  * @param params.population Population to be reproduced
  * @param params.sigma Sigma value used in mutation function
  * @param params.mutation Mutation function to be applied to chromosome
- * @param params.fitness Fitness function to be applied to child and parent to select parent or children
  *
  * @returns {population}
  */
 module.exports = function reproduction (params) {
 
-	var newPopulation = population({ paramsSize: params.population.getParamsSize() });
+	var newPopulation = population({ fitness: params.population.getFitnessFunction() });
 
 	for (var i = 0; i < params.population.getSize(); ++i) {
 		var parentChromosome = params.population.getChromosome(i);
+		// TODO: think about the mutation inside the chromosome
 		var childChromosome = params.mutation({ chromosome: parentChromosome, sigma: params.sigma });
 
-		var parentFitness = params.fitness(parentChromosome.getVariables());
-		var childFitness = params.fitness(childChromosome.getVariables());
+		var parentFitness = parentChromosome.getFitness();
+		var childFitness = childChromosome.getFitness();
 		if (childFitness > parentFitness) {
 			newPopulation.addChromosome(childChromosome);
 		} else {
@@ -28,5 +28,4 @@ module.exports = function reproduction (params) {
 		}
 	}
 	return newPopulation;
-
 };
