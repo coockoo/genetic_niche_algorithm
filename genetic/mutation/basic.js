87955f7f5bba2abdb10299909b4e195d4cc81dd1
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2015-05-28 09:14:24 +0300

    Added chromoome fitness to params in mutation

--- genetic/mutation/basic.js
+++ genetic/mutation/basic.js
@@ -3,7 +3,7 @@ var normalDistribution = require('../random/box-muller');
 
 module.exports = function basic (params) {
 	var chromosomeSize = params.chromosome.getSize();
-	var mutated = chromosome({ size: chromosomeSize });
+	var mutated = chromosome({ size: chromosomeSize, fitness: params.chromosome.getFitnessFunction() });
 	for (var i = 0; i < chromosomeSize; ++i) {
 		mutated.setVariable({ index: i, variable: params.chromosome.getVariable(i) + params.sigma * normalDistribution()});
 	}
